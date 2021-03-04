7f6a93ff2c99b6329ddd937472d2873ce9c52f9c
Dmitrii228 <200464709@student.georgianc.on.ca>
2021-03-04 13:25:34 -0500

    Initial commit

---
+++ W21COMP1008S2Test2Student/src/Car.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Car {
+    private String make, model;
+    private int mileage;
+    private double price;
+
+    public Car(String make, String model, int mileage, double price) {
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setMake(String make) {
+    }
+
+    public void setModel(String model) {
+    }
+
+    public void setMileage(int mileage) {
+    }
+
+    public void setPrice(double price) {
+    }
+
+    public String toString()
+    {
+        return "";
+    }
+}
